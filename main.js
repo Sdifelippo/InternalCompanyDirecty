6d437e7fe209324d0132c77b4c073cf64a704529
Steven Difelippo <matrixdriver04@gmail.com>
2017-07-13 18:06:25 -0400

    final

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ for (let i = 0; i < customers.results.length; i++) {
   <div class = "container">
       <img src= ${customers.results[i].picture.large}>
       <h2>${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
-      <a href="email">${customers.results[i].email}</a>
+      <a>${customers.results[i].email}</a>
 
       <div class = "info">${customers.results[i].location.street}</div>
-<div class = "info">${customers.results[i].location.city}</div>
-<div class = "info">${customers.results[i].location.state}</div>
-<div class = "info">${customers.results[i].location.postcode}</div>
+<div class = "loc">${customers.results[i].location.city},
+ ${customers.results[i].location.state},
+ ${customers.results[i].location.postcode}</div>
+
 <div class = "info">${customers.results[i].phone}</div>
+<div class = "ssn">${customers.results[i].id.value}
 </div>
 
 `;
