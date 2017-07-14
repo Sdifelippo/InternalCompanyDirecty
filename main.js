6a6cb4e1074395fb3f28e2d399849dfaf55c50b2
Steven Difelippo <matrixdriver04@gmail.com>
2017-07-13 22:22:29 -0400

    codecleanup

--- main.js
+++ main.js
@@ -6,14 +6,22 @@ for (let i = 0; i < customers.results.length; i++) {
       <h2>${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
       <a>${customers.results[i].email}</a>
 
-      <div class = "info">${customers.results[i].location.street}</div>
-<div class = "loc">${customers.results[i].location.city},
- ${customers.results[i].location.state},
- ${customers.results[i].location.postcode}</div>
+    <div class = "info">
+      ${customers.results[i].location.street}
+    </div>
 
-<div class = "info">${customers.results[i].phone}</div>
-<div class = "ssn">${customers.results[i].id.value}
-</div>
+    <div class = "loc">
+      ${customers.results[i].location.city},
+      ${customers.results[i].location.state},
+      ${customers.results[i].location.postcode}
+    </div>
+
+    <div class = "info">
+      ${customers.results[i].phone}
+    </div>
+    <div class = "ssn">
+      ${customers.results[i].id.value}
+    </div>
 
 `;
   document.querySelector('body').innerHTML += employees;
